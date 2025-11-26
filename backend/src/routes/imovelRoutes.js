53a960a3614a2d6cc602b0d1627e0562ac55013b
josue_dev <josuesa220617@gmail.com>
2025-11-26 10:07:08 -0300

    feat: rotas para buscar imovel pelo id

--- backend/src/routes/imovelRoutes.js
+++ backend/src/routes/imovelRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { listarImoveis } from "../controllers/imovelController.js";
+import { listarImoveis, buscarImovelPorId } from "../controllers/imovelController.js";
 
 const router = express.Router();
 
 // GET /api/imoveis
 router.get("/imoveis", listarImoveis);
+
+// GET /api/imoveis/:id
+router.get("/imoveis/:id", buscarImovelPorId);
 
 export default router;
